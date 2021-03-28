587b643c951897bed6301e7f6c96e537bae45022
Vlad Prozapas <fadetoblack1@yandex.ru>
2021-03-28 23:55:55 +0300

    Upd

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plot
 
-squares = [1, 1, 2, 4, 8, 16, 32, 64]
-plot.plot(squares)
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plot.plot(input_values, squares, linewidth=5)
+plot.title("Square Numbers", fontsize=24)
+plot.xlabel("Value", fontsize=14)
+plot.ylabel("Square of Value", fontsize=14)
+plot.tick_params(axis='both', labelsize=14)
+plot.scatter(2, 4)
 plot.show()
